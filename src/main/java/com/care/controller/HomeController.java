483092fa172ff69b6f81d8077ea40f0f6aca571b
jang <rechardjang@naver.com>
2020-06-08 15:07:35 +0900

    rere2

---
+++ src/main/java/com/care/controller/HomeController.java
@@ -0,0 +1,48 @@
+package com.care.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 *  '/'는 경로가 없다 거기까지다라는 말을 표현하는 벨류값이다. 마지막에 리턴해주는데가 jsp페이지라고 보면된다.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@RequestMapping("test") //경로가되고
+	public String test() {
+		
+		//db연결하는 코드
+		//...
+		return "aaa"; //aaa.jsp파일을 보여주세요 라고 보면된다.
+	}
+	
+}
